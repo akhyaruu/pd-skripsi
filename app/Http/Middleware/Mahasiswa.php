f8b0cfa1e65acf5cd516f399ac206e6a5c38fd0e
ilham akhyar <ilhamakhyar202@gmail.com>
2023-01-15 19:47:02 +0700

    add Admin, Dosen, and Mahasiswa Middleware files

--- app/Http/Middleware/Mahasiswa.php
+++ app/Http/Middleware/Mahasiswa.php
@@ -8,19 +8,19 @@ use Illuminate\Support\Facades\Auth;
 
 class Mahasiswa
 {
-   /**
-    * Handle an incoming request.
-    *
-    * @param  \Illuminate\Http\Request  $request
-    * @param  \Closure  $next
-    * @return mixed
-    */
-   public function handle(Request $request, Closure $next)
-   {
-      if (Auth::user()->role_id == 3) {
-         return $next($request);
-      } else {
-         return abort(404);
-      }
-   }
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::user()->role_id == 3) {
+            return $next($request);
+        } else {
+            return abort(404);
+        }
+    }
 }
